7b76fa51d1dc28598455bf57e65921434965bb60
anna97490 <anna.cousin97490@gmail.com>
2021-11-25 11:01:22 +0100

    update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-// Tranformer JSON en JS
+// Tranformer la chaine de caractère en JSON
 let productsInCart = JSON.parse(localStorage.getItem('products'));
 console.log(1, productsInCart);
 
@@ -13,103 +13,83 @@ postDataForm();
 function getInCart() {
     const cartItemId = document.querySelector('#cart__items');
     // Si le panier est vide
-    if (productsInCart === null) {
-        cartItemId.innerHTML = 'Votre panier est vide';
+    if (productsInCart === null || productsInCart == 0) {
+        cartItemId.innerText = 'Votre panier est vide';
     } else {
-       
-        /*productsInCart = [...productsInCart.reduce((acc, curr) => {
-            console.log(2,...productsInCart);
-
-            // Création d'une constante contentant l'id et la couleur de l'article
-            const key = curr.articleId + '-' + curr.articleColor;
-            console.log(3, curr.articleId);
-
-            // Création d'un nouvel objet avec quantity à 0 
-            const item = acc.get(key) || Object.assign({}, curr, {
-            articleQuantity: 0,
-            });console.log(4,item)
-
-            // Ajout des nouvelles qtés
-            item.articleQuantity += curr.articleQuantity;
-
-            // Actualisation de la qté en la retournant
-            return acc.set(key, item);
-            }, new Map).values()];*/
-            let productArrayCopy = [];
-            console.log(productArrayCopy)
-            // Vérifier si larticle id et la couleur sont les memes ou pas 
-            productsInCart.forEach(product => {
-                // Si l'article ajouté n'a pas le meme id et color alors il est ajouté au panier
-                if(compareCurrProduct(productArrayCopy, product)) {
-                    productArrayCopy.push(product);
-                    console.log(666,compareCurrProduct(productArrayCopy, product))
-                // Mais si c'est le cas c'est juste la qté qui change
-                } else {
-                    productArrayCopy.forEach(currProduct => {
-                        if(product.articleId === currProduct.articleId && product.articleColor === currProduct.articleColor) {
-                            currProduct.articleQuantity = currProduct.articleQuantity + product.articleQuantity;
-                        }
-                    });
-                }
-            });
-            productsInCart = productArrayCopy;
-            
-        // Generer les elements du panier sur le DOM
+        let productArrayCopy = [];
+        console.log(2,productArrayCopy);
+        // Vérifier si l'id et la couleur sont les memes ou pas 
         productsInCart.forEach(product => {
-            // Injecter l'article
+            // Si l'article ajouté n'a pas le meme id et color alors il est ajouté au panier
+            if(compareCurrProduct(productArrayCopy, product)) {
+                productArrayCopy.push(product);
+                console.log(3,compareCurrProduct(productArrayCopy, product))
+            // Mais si c'est le cas c'est juste la qté qui change
+            } else {
+                productArrayCopy.forEach(currProduct => {
+                    if(product.articleId === currProduct.articleId && product.articleColor === currProduct.articleColor) {
+                        currProduct.articleQuantity = currProduct.articleQuantity + product.articleQuantity;
+                    }
+                });
+            }
+        });
+        productsInCart = productArrayCopy;
+            
+        // Générer les éléments du panier dans DOM
+        productsInCart.forEach(product => {
+            // L'<article>
             let elementArticle = document.createElement('article');
             document.querySelector('#cart__items').appendChild(elementArticle);
             elementArticle.classList.add = 'cart__item';
             elementArticle.setAttribute('data-id', `${product.articleId}-${product.articleColor}`);
 
-
-            // Injecter la div cart__item__img
+            // La div contenant l'image
             let elementDivImg = document.createElement('div');
             elementArticle.appendChild(elementDivImg);
             elementDivImg.classList.add('cart__item__img');
 
-            // Injecter l'image dans la div 'cart__item__img'
+            // L'image 
             let elementImg = document.createElement('img');
             elementDivImg.appendChild(elementImg);
             elementImg.src = product.articleImg;
             elementImg.alt = product.articleAltTxt;
 
-            // Injecter la div 'cart__item__content'
+            // La div 'cart__item__content'
             let elementDivContent = document.createElement('div');
             elementArticle.appendChild(elementDivContent);
             elementDivContent.classList.add('cart__item__content');
 
-            // Injecter 'cart__item__content__titlePrice'
+            // La div contenant le prix et le nom du produit
             let elementDivContentTitlePrice = document.createElement('div');
             elementDivContent.appendChild(elementDivContentTitlePrice);
             elementDivContentTitlePrice.classList.add('cart__item__content__titlePrice');
 
-            // Injecter le h2
+            // Le nom du produit
             let elementTitle = document.createElement('h2');
             elementDivContentTitlePrice.appendChild(elementTitle);
             elementTitle.innerHTML = product.articleName;
 
-            // Injecter l'element <p> contenant le prix
+            // Le prix
             let elementPrice = document.createElement('p');
             elementDivContentTitlePrice.appendChild(elementPrice);
             elementPrice.innerHTML = product.articlePrice + " €";
 
-            // Injecter la div 'cart__item__content__settings'
+            // La div 'cart__item__content__settings'
             let elementDivSettings = document.createElement('div');
             elementDivContent.appendChild(elementDivSettings);
             elementDivSettings.classList.add('cart__item__content__settings');
 
-            // Injecter la div 'cart__item__content__settings__quantity'
+            // La div contenant le texte de qté et la couleur
             let elementDivSettingsQty = document.createElement('div');
             elementDivSettings.appendChild(elementDivSettingsQty);
             elementDivSettingsQty.classList.add('cart__item__content__settings__quantity');
 
-            // Injecter l'élement <p> contenant la couleur
+            // La couleur
             let elementColor = document.createElement('p');
             elementDivSettingsQty.appendChild(elementColor);
             elementColor.innerHTML = product.articleColor;
 
-            // Injecter l'element <p> contenant le texte de quantité
+            // Le texte de qté
             let elementQty = document.createElement('p');
             elementDivSettingsQty.appendChild(elementQty);
             elementQty.innerHTML = 'Quantité : ';
@@ -124,13 +104,12 @@ function getInCart() {
             elementQtyInput.setAttribute('min', "1");
             elementQtyInput.setAttribute('max', '100')
 
-
-            //Créer la div 'cart__item__content__settings__delete' 
+            // La div contenant l'option de suppression
             let elementDelete = document.createElement('div');
             elementDivSettings.appendChild(elementDelete);
             elementDelete.classList.add('cart__item__content__settings__delete'); 
 
-            //Créer l'élément '<p>' 'deleteItem' 
+            // L'élément de suppression
             let elementDeleteItm = document.createElement('p');
             elementDelete.appendChild(elementDeleteItm);
             elementDeleteItm.className = 'deleteItem';
@@ -140,14 +119,14 @@ function getInCart() {
     deleteItem();
 }
 
-//Fonction qui permettra de comparer un nouveau tableau à celui contenant les produits du LS
-function compareCurrProduct(productArray, currProduct){
+// Fonction qui permettra de comparer un nouveau tableau à celui contenant les produits du LS
+function compareCurrProduct(productArray, currProduct) {
     let exist = true;
     // Si le tableau n'est pas vide
     if(productArray.length > 0) {
         // Si au moins un élément existe 
         exist = !productArray.some(product => currProduct.articleId === product.articleId && currProduct.articleColor === product.articleColor);   
-    }console.log(333,exist)
+    }console.log(4,exist)
     return exist;  
 }
 
@@ -160,12 +139,10 @@ function totalItems() {
     const totalQty = productsInCart.reduce((accumulator, current) => accumulator + parseInt(current.articleQuantity, 10), 0);
     elementTotalQty.innerHTML = totalQty;
     
-
     // Ajouter le prix total
     let elementTotalPrice = document.getElementById('totalPrice');
     const totalPrice = productsInCart.reduce((accumulator, current) => accumulator + current.articlePrice * current.articleQuantity, 0);
-    elementTotalPrice.innerHTML = totalPrice;
-    
+    elementTotalPrice.innerHTML = totalPrice;   
 }
 
 // Changer la quantité d'articles via le panier
@@ -178,56 +155,58 @@ function qtyInput() {
            
             // Récupérer l'id et la couleur séparés par un -
             const dataItem = event.target.closest("article").dataset.id.split('-');
+            
             // Récupérer la valeur de l'input, donc la qté contenue à l'intérieur
             const newQty = event.target.value; // C'est une référence à l'objet qui a envoyé l'événement
 
            // Actualiser productsInCart
             productsInCart.forEach(item => {
                 
-                // Si la valeur de articleId = à celle de l'input et que la valeur de articleColor = à celle de l'input
+                // Si l'artcileId est = à celui de l'input et que l'articleColor = à celle de l'input
                 if(item.articleId === dataItem[0] && item.articleColor === dataItem[1]) {
                     item.articleQuantity = newQty; // Alors on change la quantité de productsInCart en la qté de l'input 
-                    
-                };   
+                }
             });
              
             // Actualiser le total
             totalItems();
-            //console.log(23,productsInCart);
         });
     
-    })
+    });
 }
 
-//La suppression d'un article
+//La suppression d'une ligne d'articles article
 function deleteItem(){
     // Récuperation des balises p "Supprimer"
     const deleteButtons = document.querySelectorAll('.deleteItem');
 
-    // Récupérations des données du bouton 'supprimer'
+    // Récupération des données de l'élément <p> 'supprimer'
     deleteButtons.forEach(btn => {
         // Pour chaque bouton supprimé, ajout d'un eventListener
         btn.addEventListener('click', (event) => {
-
+            event.preventDefault();
             // Récupération de l'id et de la couleur via l'<article>
             const dataItem = event.target.closest("article").dataset.id.split('-');
-
+            console.log(5, dataItem);
             if(dataItem.length) {
                 // Suppression de l'article correspondant
                 productsInCart = productsInCart.filter(item => item.articleId !== dataItem[0] || item.articleColor !== dataItem[1]);
+                console.log(6,productsInCart);
+                
                 // Suppression des anciens articles
                 const itemsContainer = document.getElementById('cart__items');
-                while(itemsContainer.firstChild) {
-                    // La liste n'est pas une copie, elle sera donc réindexée à chaque appel
-                    itemsContainer.removeChild(itemsContainer.firstChild);
+                console.log(7,itemsContainer);
+                while(itemsContainer.firstChild) {  
+                    itemsContainer.removeChild(itemsContainer.firstChild);  
+                       
                 }
                 // Actualisation de la liste d'article 
                 getInCart();
                 // Actualisation du total
                 totalItems(); 
                 // Actualisation du localStorage
-                localStorage.setItem('products', JSON.stringify(productsInCart));
-            }    
+                localStorage.setItem('products', JSON.stringify(productsInCart));    
+            }   
             //Ajout d'une boîte de dialogue "alert"
             alert('Votre article a bien été supprimé')
         });
@@ -237,20 +216,21 @@ function deleteItem(){
 // Validation du formulaire
 function orderForm() {
     const form = document.querySelector('.cart__order__form');
-
+    
     // Ecouter la modification du Prénom
     form.firstName.addEventListener('change', function(){
-        validFirstName(this);
+        validFirstName(this);// Fonction qui écoute l'élément en cours d'écoute .this correspond à form.firstName
     });
 
-    // Création de la reg exp pour la validation du prénom
+    // Création de fonction qui a pour paramètre form.firstName
     const validFirstName = function(inputFirstName) {
+        //Valider le format du prénom
         let regExpFirstName = new RegExp('^[a-zA-Z-]+$');
        
         // Récupération de la balise <p> '#firstNameErrorMsg'
         let firstName = inputFirstName.nextElementSibling;
 
-         // Test de l'expression régulière
+         // Test de ce que l'utilisateur a saisi
         if(regExpFirstName.test(inputFirstName.value)) {
             firstName.innerHTML = '';
         } else {
@@ -265,7 +245,7 @@ function orderForm() {
 
     // Création de la reg exp pour la validation du nom
     const validLastName = function(inputLastName) {
-        let regExpLastName = new RegExp('^[a-zA-Z-]+$');//'^([a-zA-Z])+?(['-']{1})$'
+        let regExpLastName = new RegExp('^[a-zA-Z-]+$');
         
         // Récupération de la balise <p> '#lastNameErrorMsg'
         let lastName = inputLastName.nextElementSibling;
@@ -345,8 +325,9 @@ function orderForm() {
 function postDataForm(){
     const orderBtn = document.querySelector('#order');
     const formOrder = document.getElementsByClassName('cart__order__form');
+    
     formOrder[0].addEventListener('submit', (event) => {
-        // J'annule l'envoi du formulaire
+        // Annulation de l'envoi du formulaire par défaut
         event.preventDefault();
     });
     orderBtn.addEventListener('click', () => {
@@ -362,7 +343,7 @@ function postDataForm(){
         productsInCart.forEach(article => {
             productsOrdered.push(article.articleId);
         });
-        console.log(7, productsOrdered)
+        console.log(6, productsOrdered)
 
         // Créer un objet qui contient les infos du client et les produits sélectionnés 
         const order = {
@@ -374,8 +355,8 @@ function postDataForm(){
                 email : inputEmail,
             },
             products : productsOrdered,
-        };
-        console.log(8, order);
+        }
+        console.log(7, order);
 
         // Envoi de la requete POST
         const postRequest = {
@@ -387,17 +368,20 @@ function postDataForm(){
         };  
 
         fetch('http://localhost:3000/api/products/order',postRequest)
-        .then((response) => response.json()) //-----FormData()
+        .then((response) => response.json()) 
         .then((datas) => {
-            localStorage.setItem('orderId', datas.orderId);
-            console.log(9, datas);
             
-            //document.location.href = 'confirmation.html';
-            alert('Votre formulaire a bien été envoyé');
+            localStorage.setItem('orderId', datas.orderId);
+            console.log(8, datas);
+            
         })
         .catch((err) => {
             alert ("Problème réseau");  
-        })
+        });
+        // Redirection vers la page de confirmation
+        //let confirmHref = `http://127.0.0.1:5500/front/html/confirmation.html`;
+        //alert('Votre commande a bien été validée');
+        //window.location = confirmHref;  
     });
 }
 
--- front/js/product.js
+++ front/js/product.js
@@ -7,7 +7,7 @@ getProduct();
 
 // Requete API
 function getProduct() {
-    // L'id du produit est ajouté à l'URL
+    // // Appel du produit à l'API
     fetch(`http://localhost:3000/api/products/${idProduct}`)
     .then(res => res.json()) 
     .then(article => {
@@ -46,6 +46,7 @@ function getProduct() {
 // Ajout des articles dans le panier
 function addToCart(articleToCart) {
     const btnAddToCart = document.querySelector('#addToCart');
+    console.log(3, localStorage);
     // Ecouter l'évènement du clic
     btnAddToCart.addEventListener('click', () => {
         const colorSelection = document.querySelector('#colors');
@@ -54,42 +55,46 @@ function addToCart(articleToCart) {
         // Récupérer la couleur et la qté pour les sélectionner et les envoyer avec l'objet
         let colorChoice = colorSelection.value;
         let quantityChoice = qtySelection.value;
-
+        
         // Si la quantité d'articles est comprise entre 0 et 100
-        if (qtySelection.value > 0 && qtySelection.value < 100){
+        if (qtySelection.value > 0 && qtySelection.value < 100) {
             
             // Création de l'objet qui pourra etre ajouté dans le panier
             let productOptions = {
                 articleId : idProduct,
                 articleColor : colorChoice,
-                articleQuantity : Number(quantityChoice),//---------number ou parseint
+                articleQuantity : Number(quantityChoice),
                 articleName : articleToCart.name,
                 articleDescription : articleToCart.description,
                 articlePrice : articleToCart.price,
                 articleImg : articleToCart.imageUrl,
                 articleAltTxt : articleToCart.altTxt
-            }; console.log(3, productOptions);
+            }; console.log(4, productOptions);
 
             // Initialisation du local storage
             let articleLocalStorage = [];
             // Récupérer la clé 'products'
             let itemStorage = localStorage.getItem('products'); 
-            console.log(45, localStorage);
 
             // S'il y a déja des articles dans le local storage
             if (itemStorage !== null) { // S'il y a déja des articles dans le local storage, pour ne pas les écraser
-                articleLocalStorage = JSON.parse(itemStorage); // JSON à JS
+                articleLocalStorage = JSON.parse(itemStorage); // analyse la donnée JSON et construit la valeur JS
                 articleLocalStorage.push(productOptions);
-                localStorage.setItem('products', JSON.stringify(articleLocalStorage));
-                console.log(5, JSON.parse(localStorage.getItem('products')));
+                localStorage.setItem('products', JSON.stringify(articleLocalStorage)); // Actualise le local storage
             }
             else { // Si le local storage est vide
                 articleLocalStorage.push(productOptions);
-                // Transformer la chaine de caratère en objet JS
+                // Actualiser le local storage
                 localStorage.setItem('products', JSON.stringify(articleLocalStorage)); 
-                console.log(6, articleLocalStorage);
+                console.log(5, articleLocalStorage);
             };
-            alert('Votre article a bien été ajouté au panier');
+            // Redirection vers le panier
+            let productHref = `http://127.0.0.1:5500/front/html/cart.html`;
+            if (confirm('Votre produit a bien été ajouté. Souhaitez-vous accéder à votre panier?')){
+                window.location = productHref;
+            } else {
+                window.location 
+            }
         };
     });
 }
